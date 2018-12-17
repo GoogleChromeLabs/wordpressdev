1714849348b289c5d6f1ab8e88223251f4dc18d0
Felix Arntz <felixarntz@google.com>
2018-12-17 13:43:21 +0100

    Initial commit.

---
+++ config/wp-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+
+echo "Checking out WordPress trunk. See https://develop.svn.wordpress.org/trunk"
+svn co https://develop.svn.wordpress.org/trunk/ "$PWD/trunk"
+
+cd "$PWD/trunk"
+
+echo "Installing local composer packages for {$LANDO_APP_NAME}.{$LANDO_DOMAIN}, this may take several minutes."
+composer install
+
+echo "Installing local npm packages for {$LANDO_APP_NAME}.{$LANDO_DOMAIN}, this may take several minutes."
+npm install --no-bin-links
+
+echo "Initializing grunt and creating {$LANDO_APP_NAME}.{$LANDO_DOMAIN}/build/, this may take several minutes."
+grunt
+
+echo "Creating wp-config.php for {$LANDO_APP_NAME}.{$LANDO_DOMAIN}/src/ and {$LANDO_APP_NAME}.{$LANDO_DOMAIN}/build/."
+cp -f "$PWD/config/wp-config.php" "$PWD/trunk/wp-config.php"
+cp -f "$PWD/config/wp-tests-config.php" "$PWD/trunk/wp-tests-config.php"
+
+echo "Installing {$LANDO_APP_NAME}.{$LANDO_DOMAIN}."
+wp core install --url="{$LANDO_APP_NAME}.{$LANDO_DOMAIN}/build/" --quiet --title="WordPressDev" --admin_name="admin" --admin_email="admin@local.test" --admin_password="password"
+
+cd "$PWD"

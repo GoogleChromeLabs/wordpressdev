50bab2043d74635f6af7c9b1a23e0f59e56faffb
Felix Arntz <felixarntz@google.com>
2018-12-17 16:22:25 +0100

    Fix minor issues in installation script.

--- config/wp-install.sh
+++ config/wp-install.sh
@@ -6,20 +6,20 @@ svn co https://develop.svn.wordpress.org/trunk/ "$PWD/trunk"
 
 cd "$PWD/trunk"
 
-echo "Installing local composer packages for {$LANDO_APP_NAME}.{$LANDO_DOMAIN}, this may take several minutes."
+echo "Installing local composer packages for $LANDO_APP_NAME.$LANDO_DOMAIN, this may take several minutes."
 composer install
 
-echo "Installing local npm packages for {$LANDO_APP_NAME}.{$LANDO_DOMAIN}, this may take several minutes."
+echo "Installing local npm packages for $LANDO_APP_NAME.$LANDO_DOMAIN, this may take several minutes."
 npm install --no-bin-links
 
-echo "Initializing grunt and creating {$LANDO_APP_NAME}.{$LANDO_DOMAIN}/build/, this may take several minutes."
+echo "Initializing grunt and creating $LANDO_APP_NAME.$LANDO_DOMAIN/build/, this may take several minutes."
 grunt
 
-echo "Creating wp-config.php for {$LANDO_APP_NAME}.{$LANDO_DOMAIN}/src/ and {$LANDO_APP_NAME}.{$LANDO_DOMAIN}/build/."
+echo "Creating wp-config.php for $LANDO_APP_NAME.$LANDO_DOMAIN/src/ and $LANDO_APP_NAME.$LANDO_DOMAIN/build/."
 cp -f "../config/wp-config.php" "./wp-config.php"
 cp -f "../config/wp-tests-config.php" "./wp-tests-config.php"
 
-echo "Installing {$LANDO_APP_NAME}.{$LANDO_DOMAIN}."
-wp core install --url="{$LANDO_APP_NAME}.{$LANDO_DOMAIN}/build/" --quiet --title="WordPressDev" --admin_name="admin" --admin_email="admin@local.test" --admin_password="password"
+echo "Installing $LANDO_APP_NAME.$LANDO_DOMAIN."
+wp core install --url="$LANDO_APP_NAME.$LANDO_DOMAIN/build/" --quiet --title="WordPressDev" --admin_name="admin" --admin_email="admin@local.test" --admin_password="password"
 
 cd "$PWD"

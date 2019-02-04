686c72b7d4eee345a7dd7adb26070a3cdaef078f
Weston Ruter <westonruter@google.com>
2019-02-04 10:28:33 -0800

    Write wp-config.php from scrach instead of copying it

--- config/wp-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// 'wp-config.php' is managed in a central location.
-require_once dirname( dirname( __FILE__ ) ) . '/wp-config.php';
-
-/** Sets up WordPress vars and included files. */
-require_once( ABSPATH . 'wp-settings.php' );

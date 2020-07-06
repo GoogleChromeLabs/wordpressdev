7b27ef3292a43aabc4466678dac2531d0957c838
Weston Ruter <westonruter@google.com>
2020-07-05 17:37:29 -0700

    Improve xdebug-on/xdebug-off when already on/off

--- bin/xdebug-off.sh
+++ bin/xdebug-off.sh
@@ -5,5 +5,11 @@ set -e
 if [ -e /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini ]; then
   rm /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   pkill -o -USR2 php-fpm
+  echo "Xdebug disabled"
+else
+  echo "Xdebug already disabled"
 fi
-rm /app/.xdebug-enabled
+
+if [ -e /app/.xdebug-enabled ]; then
+  rm /app/.xdebug-enabled
+fi
--- bin/xdebug-on.sh
+++ bin/xdebug-on.sh
@@ -5,3 +5,4 @@ set -e
 docker-php-ext-enable xdebug
 pkill -o -USR2 php-fpm
 touch /app/.xdebug-enabled
+echo "Xdebug enabled"

6cd3e7e8f09f8713fdf732312c8c2993a37ee5a7
Weston Ruter <westonruter@google.com>
2020-11-08 13:22:44 -0800

    Only add yaml.ini if yaml installation successful

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -19,13 +19,18 @@ if [[ -z "$LANDO_MOUNT" ]]; then
     exit 1
 fi
 
+cd "$(dirname "$0")"
+
 echo "Installing Dependencies"
 set -xe
 
 apt-get update -y
 apt-get -y install libyaml-dev
-yes | pecl install yaml
-echo 'extension=yaml.so' > /usr/local/etc/php/conf.d/yaml.ini
+if yes | pecl install yaml; then
+	echo 'extension=yaml.so' > /usr/local/etc/php/conf.d/yaml.ini
+else
+	echo "Did not install yaml."
+fi
 
 apt-get install gnupg -y
 

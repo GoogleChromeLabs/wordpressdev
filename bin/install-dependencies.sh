cbde7334b7b5525152f064995c1d3fced26d96a5
thelovekesh <lovekesh.kumar@rtcamp.com>
2023-09-16 14:18:55 +0530

    Add step to install PHPUnit from phar

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -36,7 +36,7 @@ apt-get install ca-certificates curl gnupg -y
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_VERSION.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt-get update -y
 apt-get install nodejs -y
 
@@ -47,6 +47,12 @@ apt-get install yarn -y
 
 apt-get install zip -y
 apt-get install subversion -y
+
+# Download phpunit pahr based on the major version.
+if [[ ! -e /usr/local/bin/phpunit ]]; then
+	curl -L https://phar.phpunit.de/phpunit-$PHPUNIT_MAJOR.phar -o /usr/local/bin/phpunit
+	chmod +x /usr/local/bin/phpunit
+fi
 
 # Install dependencies that are unique to the user environment.
 if [ -e install-local-dependencies.sh ]; then
